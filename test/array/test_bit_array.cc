fd225d1cd18e2a7c94de8f52d06d33c54ddf0628
Smitha Milli <smitha.milli@gmail.com>
2015-06-16 14:59:17 -0700

    Change method for instantiation of objects

--- test/array/test_bit_array.cc
+++ test/array/test_bit_array.cc
@@ -5,13 +5,13 @@ const int BITS_IN_BYTE = 8;
 
 TEST_CASE("BitArray constructs correct size array", "[bit_array]") {
   long num_bits = 51028;
-  BitArray bit_array = BitArray(num_bits);
+  BitArray bit_array (num_bits);
   int expected_capacity = ceil(num_bits / (sizeof(long) * 8.0));
   REQUIRE(bit_array.get_capacity() == expected_capacity);
 }
 
 TEST_CASE("BitArray::append appends when it can fit in 1 long", "[bit_array]") {
-  BitArray bit_array = BitArray(sizeof(long) * BITS_IN_BYTE * 2);
+  BitArray bit_array (sizeof(long) * BITS_IN_BYTE * 2);
   bit_array.append(25, 8);
   bit_array.append(135, 8);
   REQUIRE(bit_array.get(0, 2) == 0);
@@ -21,7 +21,7 @@ TEST_CASE("BitArray::append appends when it can fit in 1 long", "[bit_array]") {
 }
 
 TEST_CASE("BitArray::append appends when it requires 2 longs", "[bit_array]") {
-  BitArray bit_array = BitArray(sizeof(long) * BITS_IN_BYTE * 2);
+  BitArray bit_array (sizeof(long) * BITS_IN_BYTE * 2);
   bit_array.append(138, sizeof(long) * BITS_IN_BYTE - 2);
   bit_array.append(24, 8);
   REQUIRE(bit_array.get(sizeof(long) * BITS_IN_BYTE - 2, 2) == 0);
@@ -30,7 +30,7 @@ TEST_CASE("BitArray::append appends when it requires 2 longs", "[bit_array]") {
 }
 
 TEST_CASE("BitArray resizes when necessary", "[bit_array]") {
-  BitArray bit_array = BitArray(sizeof(long) * BITS_IN_BYTE * 2);
+  BitArray bit_array (sizeof(long) * BITS_IN_BYTE * 2);
   bit_array.append(138, sizeof(long) * BITS_IN_BYTE);
   bit_array.append(4552, sizeof(long) * BITS_IN_BYTE);
   bit_array.append(789, sizeof(long) * BITS_IN_BYTE);
@@ -39,7 +39,7 @@ TEST_CASE("BitArray resizes when necessary", "[bit_array]") {
 
 TEST_CASE("BitArray::shrink_to_fit shrinks to minimum capacity necessary",
     "[bit_array]") {
-  BitArray bit_array = BitArray(sizeof(long) * BITS_IN_BYTE * 2);
+  BitArray bit_array (sizeof(long) * BITS_IN_BYTE * 2);
   bit_array.append(138, sizeof(long) * BITS_IN_BYTE);
   bit_array.append(4552, sizeof(long) * BITS_IN_BYTE);
   bit_array.append(789, sizeof(long) * BITS_IN_BYTE);
